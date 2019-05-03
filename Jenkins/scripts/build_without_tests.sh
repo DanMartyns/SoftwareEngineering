34bf48deb0a61f91bb13c5d3ddff67c88ca3cc25
Daniel Martins <danielmcoelho@ua.pt>
2019-05-03 17:23:50 +0100

    Jenkinsfile updated; Script to build without tests

---
+++ Jenkins/scripts/build_without_tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Here we will have the essential directories for the entire project 
+MODULES=(
+    ESProject
+)
+
+for i in ${!MODULES[@]}; do
+    # Go to module project folder
+    cd ${MODULES[$i]}
+    
+    # Clean old build files
+    rm -rf target
+
+    # Build without tests 
+    # mvn -X -Dmaven.test.skip=true -Dmongo_host=127.0.0.1 -Dkafka_bootstrap_servers=127.0.0.1:9092 -Ddatasource_postgres_alerts=jdbc:postgresql://127.0.0.1:5432/ -Ddatasource_postgres=jdbc:postgresql://127.0.0.1:5432/ -Ddatasource_postgres_triggered_alerts=jdbc:postgresql://127.0.0.1:5432/ clean package
+    
+    # Go again to project root folder
+    cd ..
+done
