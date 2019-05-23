a26290aa141160ea23e5987036a6f7e53093f2f2
Daniel Martins <danielmcoelho@ua.pt>
2019-05-23 14:29:34 +0100

    deploy updated

--- Jenkins/scripts/deploy.sh
+++ Jenkins/scripts/deploy.sh
@@ -27,19 +27,19 @@ done
 # Copy dashboard to deploy folder
 cp -r Dashboard/interface $DEPLOY_FOLDER
 
-# Compress folder and send it to the deploy machine
-tar -zcvf $DEPLOY_FOLDER.tgz $DEPLOY_FOLDER
-scp -o StrictHostKeyChecking=no $DEPLOY_FOLDER.tgz $MACHINE_USERNAME@$MACHINE_HOSTNAME:~/$DEPLOY_PATH
+# # Compress folder and send it to the deploy machine
+# tar -zcvf $DEPLOY_FOLDER.tgz $DEPLOY_FOLDER
+# scp -o StrictHostKeyChecking=no $DEPLOY_FOLDER.tgz $MACHINE_USERNAME@$MACHINE_HOSTNAME:~/$DEPLOY_PATH
 
-# Decompress builded modules and restart docker container
-sshpass -e ssh -o StrictHostKeyChecking=no $MACHINE_USERNAME@$MACHINE_HOSTNAME 
-	"mkdir -p $DEPLOY_PATH &&
-	rm -rf $DEPLOY_PATH/$DEPLOY_FOLDER &&
-	tar -zxvf $DEPLOY_PATH/$DEPLOY_FOLDER.tgz -C $DEPLOY_PATH &&
-	docker restart softwareengineering_main_1 &&
-	docker restart softwareengineering_dashboard_1 &&
-	rm $DEPLOY_PATH/$DEPLOY_FOLDER.tgz"
+# # Decompress builded modules and restart docker container
+# sshpass -e ssh -o StrictHostKeyChecking=no $MACHINE_USERNAME@$MACHINE_HOSTNAME 
+# 	"mkdir -p $DEPLOY_PATH &&
+# 	rm -rf $DEPLOY_PATH/$DEPLOY_FOLDER &&
+# 	tar -zxvf $DEPLOY_PATH/$DEPLOY_FOLDER.tgz -C $DEPLOY_PATH &&
+# 	docker restart softwareengineering_main_1 &&
+# 	docker restart softwareengineering_dashboard_1 &&
+# 	rm $DEPLOY_PATH/$DEPLOY_FOLDER.tgz"
 
-# Clean up things
-rm -rf $DEPLOY_FOLDER
-rm $DEPLOY_FOLDER.tgz
+# # Clean up things
+# rm -rf $DEPLOY_FOLDER
+# rm $DEPLOY_FOLDER.tgz
