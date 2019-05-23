44780651490d0bd34c85c66e5cd1fe9d985545e9
Daniel Martins <danielmcoelho@ua.pt>
2019-05-23 19:40:38 +0100

    server.sh updated

--- Jenkins/scripts/server.sh
+++ Jenkins/scripts/server.sh
@@ -3,6 +3,8 @@ RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${2:-"OK"}\r\n"
 while { echo -en "$RESPONSE"; } | nc -l "${1:-8080}"; do
 	rm -f SoftwareEngineering  
 	git clone https://github.com/DanMartyns/SoftwareEngineering.git > logGit.txt
-	cd SoftwareEngineering 
+	cd SoftwareEngineering
+	mvn -f MainProject/dockerdemo/pom.xml -Dmaven.test.failure.ignore=false package
+	docker-compose build  
 	docker-compose up > logCompose.txt 
 done
